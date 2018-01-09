fb93b14413901e57d61712cdc28651a38c6916d8
Pawel Skaruz <paw3lx@gmail.com>
2018-01-09 18:56:19 +0100

    added RegisterValit extension

---
+++ src/ContainerBuilderExtensions.cs
@@ -0,0 +1,17 @@
+using System.Reflection;
+using Autofac;
+
+namespace Valit.Autofac
+{
+    public static class ContainerBuilderExtensions
+    {
+        public static ContainerBuilder RegisterValit(this ContainerBuilder builder)
+        {
+            var assembly = Assembly.GetCallingAssembly();
+            builder.RegisterAssemblyTypes(assembly)
+                .AsClosedTypesOf(typeof(IValitator<>))
+                .AsImplementedInterfaces();
+            return builder;
+        }
+    }
+}
